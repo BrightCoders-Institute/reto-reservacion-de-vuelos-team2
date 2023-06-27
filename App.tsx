7b015e995562dd0dbdc89dc2ce6d623316651cdf
Alfredo20164920 <apaz20164920@gmail.com>
2023-06-26 18:55:23 -0600

    Feat: get data from a test db
    Co-Authored-By: AlbertoG22 <albertogarciamartinez99@gmail.com>
    Co-authored-by: Montserrat Aguilar <aguilarvalle.montse@gmail.com>

--- App.tsx
+++ App.tsx
@@ -5,10 +5,24 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import {collection, getDocs} from 'firebase/firestore';
+import {database} from './src/db/firebase';
+
+async function fethData() {
+  const querySnapshot = await getDocs(collection(database, 'users'));
+  querySnapshot.forEach(doc => {
+    const {first_name, email, password} = doc.data();
+    console.log(`${doc.id} => ${first_name}, ${email}, ${password}`);
+  });
+}
 
 function App(): JSX.Element {
+  useEffect(() => {
+    fethData();
+  }, []);
+
   return (
     <SafeAreaView>
       <Text>Hola mundo</Text>
