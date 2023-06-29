e7862601bf9b7bf4cfe67735bb2931c74e1b34f6
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-06-29 10:36:05 -0600

    fix(core): fix screen SignUp
    Co-Authored-By: AlbertoG22 <albertogarciamartinez99@gmail.com>
    Co-Authored-by: Alfredo20164920 <apaz20164920@gmail.com>

--- src/components/CheckBoxComponent.jsx
+++ src/components/CheckBoxComponent.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { styles } from '../styles/AppStyles';
-import { Text, View } from 'react-native';
+import {styles} from '../styles/AppStyles';
+import {View} from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
 
-export const CheckBoxComponent = ({ checkboxValue, onCheckboxChange, tintColors, children }) => {
+export const CheckBoxComponent = ({
+  checkboxValue,
+  onCheckboxChange,
+  tintColors,
+  children,
+}) => {
   return (
     <View style={styles.checkboxItemContainer}>
-        <CheckBox
-            disabled={false}
-            value={checkboxValue}
-            tintColors={tintColors}
-            onValueChange={onCheckboxChange}
-        />
-        {children}
+      <CheckBox
+        disabled={false}
+        value={checkboxValue}
+        tintColors={tintColors}
+        onValueChange={onCheckboxChange}
+      />
+      {children}
     </View>
   );
 };
--- src/components/TextFieldForm.jsx
+++ src/components/TextFieldForm.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import { Text, View, TextInput, TouchableOpacity } from 'react-native';
-import { styles } from '../styles/AppStyles';
+import {Text, View, TextInput, TouchableOpacity} from 'react-native';
+import {styles} from '../styles/AppStyles';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-
-export const TextFieldForm = ({ inputTitle, inputValue, onInputChange, extraData }) => {
-
+export const TextFieldForm = ({
+  inputTitle,
+  inputValue,
+  onInputChange,
+  extraData,
+}) => {
   const textInputContent = () => {
     let inputFieldContent = <></>;
-    if(inputTitle === 'Password *') {
+    if (inputTitle === 'Password *') {
       inputFieldContent = (
         <View style={styles.inputFieldContainer}>
           <TextInput
             style={styles.inputField}
-            value={ inputValue }
-            onChangeText={ onInputChange }
+            value={inputValue}
+            onChangeText={onInputChange}
             secureTextEntry={!extraData.showPassword}
           />
           <TouchableOpacity
             onPress={extraData.handleShowPassword}
-            style={styles.iconContainer}
-          >
+            style={styles.iconContainer}>
             <Icon
               name={extraData.showPassword ? 'eye' : 'eye-off'}
               size={25}
@@ -33,26 +35,19 @@ export const TextFieldForm = ({ inputTitle, inputValue, onInputChange, extraData
       inputFieldContent = (
         <TextInput
           style={styles.inputField}
-          value={ inputValue }
-          onChangeText={ onInputChange }
+          value={inputValue}
+          onChangeText={onInputChange}
         />
       );
     }
 
     return inputFieldContent;
-  }
+  };
 
   return (
     <View style={styles.fieldContainer}>
-
-      <Text style={styles.textInput}>{ inputTitle }</Text>
-      {/* <TextInput
-        style={styles.inputField}
-        value={ inputValue }
-        onChangeText={ onInputChange }
-      /> */}
-      { textInputContent() }
-
+      <Text style={styles.textInput}>{inputTitle}</Text>
+      {textInputContent()}
     </View>
   );
 };
