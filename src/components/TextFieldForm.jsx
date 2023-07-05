a70199eae16a9b019c44119a7e8309316fe7cb89
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-07-04 20:30:05 -0600

    Signup validation for email and password fields, enable/disable signup buttons
    Co-authored-by: Montserrat Aguilar <aguilarvalle.montse@gmail.com>
    Co-authored-by: Alfredo20164920 <apaz20164920@gmail.com>

--- src/components/TextFieldForm.jsx
+++ src/components/TextFieldForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Text, View, TextInput, TouchableOpacity} from 'react-native';
 import {styles} from '../styles/AppStyles';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -8,19 +8,45 @@ export const TextFieldForm = ({
   inputValue,
   onInputChange,
   extraData,
+  invalidText,
+  isInputValid = true,
+  setInputValid = undefined
 }) => {
+
+  const [isFocus, setIsFocus] = useState(false);
+
+  useEffect(() => {
+    if(setInputValid) {
+      setInputValid(true);
+    }
+  }, []);
+
+  const handleFocus = () => {
+    setIsFocus(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocus(false);
+  };
+
   const textInputContent = () => {
+
     let inputFieldContent = <></>;
     if (inputTitle === 'Password *') {
       inputFieldContent = (
         <View style={styles.inputFieldContainer}>
+
           <TextInput
-            style={styles.inputField}
+            style={[styles.inputField, isFocus || (inputValue.trim() !== '') ? styles.focus : '']}
             value={inputValue}
             onChangeText={onInputChange}
             secureTextEntry={!extraData.showPassword}
             autoCapitalize="none"
+            onBlur={handleBlur}
+            onFocus={handleFocus}
+            cursorColor='#5C65B1'
           />
+
           <TouchableOpacity
             onPress={extraData.handleShowPassword}
             style={styles.iconContainer}>
@@ -35,10 +61,13 @@ export const TextFieldForm = ({
     } else {
       inputFieldContent = (
         <TextInput
-          style={styles.inputField}
+          style={[styles.inputField, isFocus || (inputValue.trim() !== '') ? styles.focus : '']}
           value={inputValue}
           onChangeText={onInputChange}
           autoCapitalize="none"
+          onBlur={handleBlur}
+          onFocus={handleFocus}
+          cursorColor='#5C65B1'
         />
       );
     }
@@ -48,7 +77,10 @@ export const TextFieldForm = ({
 
   return (
     <View style={styles.fieldContainer}>
-      <Text style={styles.textInput}>{inputTitle}</Text>
+      <View style={{display: 'flex', flexDirection: 'row', alignItems: 'baseline'}}>
+        <Text style={styles.textInput}>{ inputTitle }</Text>
+        { isInputValid === false ? <Text style={[styles.feedbackValidation]}>{ invalidText }</Text> : ''}
+      </View>
       {textInputContent()}
     </View>
   );
