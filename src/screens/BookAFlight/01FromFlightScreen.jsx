235fa52f6182a92fc722d994945d4da50f831bbe
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-07-12 18:54:46 -0600

    airports firebase

--- src/screens/BookAFlight/01FromFlightScreen.jsx
+++ src/screens/BookAFlight/01FromFlightScreen.jsx
@@ -1,16 +1,57 @@
-import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, FlatList} from 'react-native';
 import {styles} from '../../styles/AppStyles';
 import {TitleFlightComponent} from '../../components/BookAFlight/TitleFlightComponent';
 import {useForm} from '../../hooks/useForm';
 import {TextFieldFlight} from '../../components/BookAFlight/TextFieldFlight';
 import {ButtonFlightComponent} from '../../components/BookAFlight/ButtonFlightComponent';
+import firestore from '@react-native-firebase/firestore';
 
 export const FromFlightScreen1 = ({navigation}) => {
   const [isDesabledSignupBtn, setIsDesabledSignupBtn] = useState(true);
   const {fromField, onResetForm, onInputChange} = useForm({
     fromField,
   });
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    getCities();
+  }, []);
+
+  const handleSearch = async (text) => {
+    onInputChange(text);
+
+    // Realiza la búsqueda en Firebase
+    const snapshot = await firebase
+      .database()
+      .ref('registros')
+      .orderByChild('campoDeBusqueda')
+      .startAt(text)
+      .endAt(text + '\uf8ff')
+      .once('value');
+
+    // Obtén los resultados de búsqueda
+    const data = snapshot.val();
+    const results = data ? Object.values(data) : [];
+
+    setSearchResults(results);
+  };
+
+  const getCities = async () => {
+    const arrayAirports = [];
+    const originPlaces = await firestore().collection('airports').get();
+    const airports = originPlaces.docs;
+    for (const i of airports) {
+      // console.log(i._data);
+      arrayAirports.push(i._data);
+    }
+    console.log(arrayAirports);
+    setSearchResults(arrayAirports);
+  };
+
+  const handleInput = async (text) => {
+    console.log(text);
+  };
 
   return (
     <View style={styles.fromFlightContainer}>
@@ -21,10 +62,19 @@ export const FromFlightScreen1 = ({navigation}) => {
         marginTop={40}
       />
 
-      <TextFieldFlight
-        inputPlaceholder="Select location"
-        inputValue={fromField}
-      />
+      <View style={{display: 'flex', flexDirection: 'column'}}>
+        <TextFieldFlight
+          inputPlaceholder="Select location"
+          inputValue={fromField}
+          onChangeTextFn={handleInput}
+        />
+        {/* <FlatList
+          data={searchResults}
+          renderItem={({item}) => <Text>{item.country}</Text>}
+          style={{backgroundColor: 'red'}}
+          keyExtractor={(item) => item.id}
+        /> */}
+      </View>
 
       <ButtonFlightComponent
         onPressFn={() => navigation.navigate('ToScreen')}
